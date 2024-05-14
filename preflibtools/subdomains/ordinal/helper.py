a00727357e5615171f9ced938d6d39cde9cc3cab
Lotte Klomp <lotte.klomp@student.uva.nl>
2024-05-14 16:07:17 +0200

    Added functions used in SC_voter_deletion.py

--- preflibtools/subdomains/ordinal/helper.py
+++ preflibtools/subdomains/ordinal/helper.py
@@ -7,7 +7,7 @@ def get_alternatives(instance: OrdinalInstance):
         instance (OrdinalInstance): the instance
 
     Returns:
-        set: the set of alternatives
+        (set): the set of alternatives
     """
     alternatives = set()
     for alt,  _ in instance.alternatives_name.items():
@@ -22,7 +22,7 @@ def get_voters(instance: OrdinalInstance):
         instance (OrdinalInstance): the instance
 
     Returns:
-        list: the list of voters
+        (list): the list of voters
     """
     voters = []
 
@@ -45,3 +45,36 @@ def restrict_preferences(instance: OrdinalInstance, alternatives_set: set):
         preferences.append(pref)
 
     return preferences
+
+def get_num_equal_voters(instance: OrdinalInstance, vote: list):
+    """Return the number of voters with the same preference order as the given
+    vote.
+
+    Args:
+        instance (OrdinalInstance): the preference profile.
+        vote (list): the preference order to compare.
+
+    Returns:
+        (int): the number of voters with the same preference order as the given
+               vote.
+    """
+    vote_map = instance.vote_map()
+    vote = tuple([(v, ) for v in vote])
+    return vote_map[vote]
+
+def write_instance(instance: OrdinalInstance, num_voters: int,
+                   num_alternatives: int, num_unique_orders: int, name: str):
+    with open(f"{name}.soc", "w") as file:
+        file.write("# FILE NAME: " + name + "\n")
+        file.write("# NUMBER ALTERNATIVES: " + str(num_alternatives) + "\n")
+        file.write("# NUMBER VOTERS: " + str(num_voters) + "\n")
+        file.write("# NUMBER UNIQUE ORDERS: " + str(num_unique_orders) + "\n")
+
+        for i in range(num_alternatives):
+            file.write(f"# ALTERNATIVE NAME {str(i + 1)}: {chr(i + 97)}" + "\n")
+
+        # Iterate over the votes and their multiplicities
+        for vote, multiplicity in instance.multiplicity.items():
+            file.write(f"{multiplicity}: " + ", ".join([str(x[0]) for x in vote]) + "\n")
+
+        file.close()
